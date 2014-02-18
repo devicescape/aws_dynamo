1efbc36a4f4eb85a5ef24ad8e58a9a228638374e
David Kimdon <david.kimdon@devicescape.com>
2014-02-17 21:59:52 -0800

    Make create table tests more extensive.

--- test/aws_dynamo_create_table_tests.c
+++ test/aws_dynamo_create_table_tests.c
@@ -16,6 +16,8 @@
  * along with aws_dynamo.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define _GNU_SOURCE
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <assert.h>
@@ -57,37 +59,70 @@ static void test_parse_create_table(void)
 	aws_dynamo_free_create_table_response(r);
 }
 
-static void test_create_table(void)
+static void test_create_table(const char *hash_key_type, const char *range_key_type)
 {
 	struct aws_dynamo_create_table_response *r;
 	int write_units = 1;
-	int read_units = 1;
-	const char *request =
+	int read_units = 2;
+	const char *create_request_hash_format =
 "{\
-	\"TableName\":\"aws_dynamo_test_table\",\
+	\"TableName\":\"aws_dynamo_test_table_%d\",\
 	\"KeySchema\":{\
 		\"HashKeyElement\":{\
-			\"AttributeName\":\"id\",\
-			\"AttributeType\":\"N\"\
-		}},\
+			\"AttributeName\":\"hash\",\
+			\"AttributeType\":\"%s\"\
+		}\
+	},\
 	\"ProvisionedThroughput\":{\
-		\"ReadCapacityUnits\":1,\
+		\"ReadCapacityUnits\":2,\
 		\"WriteCapacityUnits\":1\
 	}\
 }";
-	const char *describe_request = "{\
-	\"TableName\":\"aws_dynamo_test_table\"\
+	const char *create_request_hash_range_format =
+"{\
+	\"TableName\":\"aws_dynamo_test_table_%d\",\
+	\"KeySchema\":{\
+		\"HashKeyElement\":{\
+			\"AttributeName\":\"hash\",\
+			\"AttributeType\":\"%s\"\
+		},\
+		\"RangeKeyElement\":{\
+			\"AttributeName\":\"range\",\
+			\"AttributeType\":\"%s\"\
+		}\
+	},\
+	\"ProvisionedThroughput\":{\
+		\"ReadCapacityUnits\":2,\
+		\"WriteCapacityUnits\":1\
+	}\
 }";
+	char *create_request = NULL;
+	const char *describe_request_format = "{\
+	\"TableName\":\"aws_dynamo_test_table_%d\"\
+}";
+	char *describe_request = NULL;
 	struct aws_dynamo_describe_table_response *describe_r;
-	const char *delete_request = "{\
-	\"TableName\":\"aws_dynamo_test_table\"\
+	const char *delete_request_format = "{\
+	\"TableName\":\"aws_dynamo_test_table_%d\"\
 }";
+	char *delete_request = NULL;
 	struct aws_handle *aws_dynamo;
 	struct aws_dynamo_delete_table_response *delete_r;
+	time_t now = time(NULL);
 
 	aws_dynamo = aws_init(NULL, NULL);
 
 	assert(aws_dynamo != NULL);
+	printf("Testing table creation: hash type %s, range type %s\n", hash_key_type, range_key_type);
+
+	if (hash_key_type && !range_key_type) {
+		assert(asprintf(&create_request, create_request_hash_format, now, hash_key_type) != -1);
+	} else if (hash_key_type && range_key_type) {
+		assert(asprintf(&create_request, create_request_hash_range_format, now, hash_key_type, range_key_type) != -1);
+	}
+
+	assert(asprintf(&describe_request, describe_request_format, now) != -1);
+	assert(asprintf(&delete_request, delete_request_format, now) != -1);
 
 	do {
 		/* Make sure the table doesn't exist. */
@@ -111,7 +146,7 @@ static void test_create_table(void)
 
 	} while (1);
 
-	r = aws_dynamo_create_table(aws_dynamo, request);
+	r = aws_dynamo_create_table(aws_dynamo, create_request);
 
 	aws_dynamo_dump_create_table_response(r);
 	aws_dynamo_free_create_table_response(r);
@@ -129,18 +164,53 @@ static void test_create_table(void)
 		}
 		assert(describe_r->write_units == write_units);
 		assert(describe_r->read_units == read_units);
+		assert(strcmp(describe_r->hash_key_name, "hash") == 0);
+		if (range_key_type) {
+			assert(strcmp(describe_r->range_key_name, "range") == 0);
+		} else {
+			assert(describe_r->range_key_name == NULL);
+		}
 		aws_dynamo_free_describe_table_response(describe_r);
 		break;
 
 	} while (1);
 
+	/* Now delete the table, we are done. */
+	delete_r = aws_dynamo_delete_table(aws_dynamo, delete_request);
+	aws_dynamo_dump_delete_table_response(delete_r);
+	aws_dynamo_free_delete_table_response(delete_r);
+
+	do {
+		/* Make sure the table doesn't exist. */
+		describe_r = aws_dynamo_describe_table(aws_dynamo, describe_request);
+
+		aws_dynamo_dump_describe_table_response(describe_r);
+		if (describe_r == NULL && aws_dynamo_get_errno(aws_dynamo) == AWS_DYNAMO_CODE_RESOURCE_NOT_FOUND_EXCEPTION) {
+			/* Good, the table isn't there. */
+			aws_dynamo_free_describe_table_response(describe_r);
+			break;
+		}
+
+		aws_dynamo_free_describe_table_response(describe_r);
+		sleep(20);
+
+	} while (1);
+
+	free(create_request);
+	free(describe_request);
+	free(delete_request);
 	aws_deinit(aws_dynamo);
 }
 
 int main(int argc, char *argv[])
 {
 	test_parse_create_table();
-	test_create_table();
+	test_create_table("N", NULL);
+	test_create_table("N", "N");
+	test_create_table("S", NULL);
+	test_create_table("S", "S");
+	test_create_table("N", "S");
+	test_create_table("S", "N");
 
 	return 0;
 }
