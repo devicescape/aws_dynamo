81a4fc68a5128a2132af3f607ff8d0400b565d9e
David Kimdon <david.kimdon@devicescape.com>
2014-04-24 17:14:18 +0000

    Cleanup working copy before build.

--- pkg/dist.sh
+++ pkg/dist.sh
@@ -1,12 +1,36 @@
 #!/bin/sh
-THISDIR="$( cd "$( dirname "$0" )" && pwd )"
+set -eux
 
 VERSION=$1
 
+THISDIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $THISDIR
 cd ..
 rm -rf ds-release
 mkdir ds-release
+
+export GIT_PAGER=''
+
+git status
+CHANGES=`git status | egrep 'modified|removed|changed|Changes|Untracked|ahead|behind'| wc -l`
+if [ "$CHANGES" != "0" ] ; then
+    echo "Cleaning up local changes."
+    git add -A
+    git diff --cached
+    git reset --hard
+    git clean -f
+    git checkout master #We are just using the master branch for now.
+    git branch
+fi
+git pull --rebase
+CHANGES=`git status | egrep 'modified|removed|changed|Changes|Untracked|ahead|behind'| wc -l`
+if [ "$CHANGES" != "0" ] ; then
+    echo "Local changes, aborting."
+    #echo "FIXME!!! Skipping changes for debugging"; exit 0
+    exit 1
+fi
+
+git log -n1 --stat
 
 chmod +w pkg/aws_dynamo.spec
 
